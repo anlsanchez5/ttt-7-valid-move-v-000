88932731713c7ee1bc31c18c26d325668b377a0f
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-08-23 15:50:40 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,14 @@
 
+def position_taken?(arr,ind)
+    check = arr[ind]
+    if check == " " || check == "" || check == nil
+       false 
+    elsif check == "X" || check == "O"
+       true
+    end
+end
 
 
 def valid_move?(board, index)
-  if board[index] != "" && board[index] != " " && board[index] != nil 
+ 
     
