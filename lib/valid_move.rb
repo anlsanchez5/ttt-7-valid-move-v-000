fdb5e7fe88fc5ca04c53048e3a3a5c14f514b2e2
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-08-23 16:00:35 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,7 +10,7 @@ end
 
 
 def valid_move?(board, index)
-  if index.between?(0,8) $$ position_taken(board, index) == false
+  if index.between?(0,8) $$ position_taken(board,index) == false
     true
   else
     false
